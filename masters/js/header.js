8c6458c8401985018372f0e8a3bc980d5ecb6cc7
Brybry16 <bryanperroud@hotmail.com>
2024-04-30 16:21:47 +0200

    Suppression de la ligne de test pour forcer l'affichage en anglais

--- masters/js/header.js
+++ masters/js/header.js
@@ -1,5 +1,4 @@
 let locale = navigator.language || navigator.userLanguage || "en-US";
-locale = "en-US";
 let lng = locale.substring(0, 2);
 if(lng !== "fr" && lng !== "en") lng = "en";
 let commonLocales;
